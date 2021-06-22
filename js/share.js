697dc3391d175e82eae59e8ea32d9ba001019448
rngueco <RiyanaGueco@gmail.com>
2021-06-22 15:30:05 +0800

    Add shadows, add button effects on click

--- js/share.js
+++ js/share.js
@@ -1,4 +1,6 @@
 function toggleShareOptions() {
     var share_options = document.querySelector('.share-options');
+    var article_button = document.querySelector('.article-card__share');
     share_options.classList.toggle('hidden');
+    article_button.classList.toggle('dark-button');
 }
